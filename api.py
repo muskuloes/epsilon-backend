8ab13813d047aa3f6d7d7b20a91d0bbcdc17802a
muskuloes <t.victor@campus.lmu.de>
2020-05-31 18:57:03 +0200

    handle multiple file uploads

--- api.py
+++ api.py
@@ -4,11 +4,13 @@ import uuid
 from flask import Flask, request
 from flask_restful import Resource, Api
 from flask_pymongo import PyMongo
+from flask_cors import CORS
 
 app = Flask(__name__)
 app.config["MONGO_URI"] = os.getenv(
     "MONGO_URI", default="mongodb://localhost:27017/hca"
 )
+CORS(app)
 mongo = PyMongo(app)
 
 api = Api(app)
@@ -24,12 +26,15 @@ class Upload(Resource):
         return mongo.send_file(filename)
 
     def post(self, **kwargs):
-        file = request.files["file"]
-        file_extension = file.filename.split(".")[-1]
-        filename = "{}.{}".format(uuid.uuid4(), file_extension)
-        mongo.save_file(filename, file)
+        files = request.files
+        filenames = []
+        for param, file in files.items(multi=True):
+            file_extension = file.filename.split(".")[-1]
+            filename = "{}.{}".format(uuid.uuid4(), file_extension)
+            filenames.append(filename)
+            mongo.save_file(filename, file)
 
-        return {"filename": filename}
+        return {"filenames": filenames}
 
 
 api.add_resource(HelloWorld, "/")
