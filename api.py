136ce562a56b0b88dae237c6966b275572458986
muskuloes <t.victor@campus.lmu.de>
2020-05-31 02:44:18 +0200

    implement upload endpoint

--- api.py
+++ api.py
@@ -1,7 +1,12 @@
-from flask import Flask
+from flask import Flask, request
 from flask_restful import Resource, Api
+from flask_pymongo import PyMongo
+import uuid
 
 app = Flask(__name__)
+app.config['MONGO_URI'] = 'mongodb://localhost:27017/hca'
+mongo = PyMongo(app)
+
 api = Api(app)
 
 
@@ -10,7 +15,21 @@ class HelloWorld(Resource):
         return {'Team': 'Unicorn'}
 
 
+class Upload(Resource):
+    def get(self, filename):
+        return mongo.send_file(filename)
+
+    def post(self, **kwargs):
+        file = request.files['file']
+        file_extension = file.filename.split('.')[-1]
+        filename = "{}.{}".format(uuid.uuid4(), file_extension)
+        mongo.save_file(filename, request.files['file'])
+
+        return {'filename': filename}
+
+
 api.add_resource(HelloWorld, '/')
+api.add_resource(Upload, '/upload', '/upload/<string:filename>')
 
 if __name__ == '__main__':
     app.run(debug=True)
